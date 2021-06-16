1e1955cc274a807f923ed0939e64bd8e3bbbcaa8
zeon77 <zeon77@gmail.com>
2021-06-16 20:54:13 +0200

    Add project files.

---
+++ vb2018/Stadion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vb2018
+{
+    class Stadion
+    {
+        public string Város { get; set; }
+        public string Név { get; set; }
+        public string AltNév { get; set; }
+        public int Férőhely { get; set; }
+
+        public Stadion(string sor)
+        {
+            string[] tmp = sor.Split(';');
+            this.Város = tmp[0];
+            this.Név = tmp[1];
+            this.AltNév = tmp[2];
+            this.Férőhely = int.Parse(tmp[3]);
+        }
+    }
+}
